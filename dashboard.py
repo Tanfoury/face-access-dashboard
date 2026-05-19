b6b17aaa020d648ef0f95159e8d29b0de7753fd3
Tanfoury <hamza.tanfouri@isimg.tn>
2026-05-19 18:00:56 +0100

    Fix import database dans dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,8 +3,8 @@ import cv2
 import streamlit as st
 import pandas as pd
 from datetime import datetime, date
-from database1 import Session, Student, AccessLog, init_db, Schedule, get_schedules
-from database1 import (
+from database import Session, Student, AccessLog, init_db, Schedule, get_schedules
+from database import (
     get_all_subjects, add_subject,
     save_time_slot, delete_time_slot,
     get_time_slots, get_schedule_grid,
